fb657df2c594e52331652fea98564f83a892624b
ChristianMG <christianmagaa@gmail.com>
2022-06-07 21:21:44 -0500

    add toolbar, bottom_nav and recycler view to home view.

---
+++ app/src/main/java/com/christian_magana/twitter_mirroring/ui/home/TweetsAdapter.kt
@@ -0,0 +1,21 @@
+package com.christian_magana.twitter_mirroring.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.christian_magana.twitter_mirroring.R
+
+class TweetsAdapter(private var tweets: List<String>):
+    RecyclerView.Adapter<TweetsHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TweetsHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return TweetsHolder(layoutInflater.inflate(R.layout.item_tweet, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: TweetsHolder, position: Int) {
+        val tweet = tweets[position]
+        holder.render(tweet)
+    }
+
+    override fun getItemCount(): Int = tweets.size
+}
